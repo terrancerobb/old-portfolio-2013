d379fae2e52aebd93c741cfcbe41af75e32bb42e
Terry Robb <terranceprobb@gmail.com>
2013-07-05 09:10:00 -0400

    Added monsterfan to landing page folder

---
+++ l/monsterfan/src/contents/lib/js/mf.js
@@ -0,0 +1,21 @@
+$(function() {
+  var thisNav = $("nav.menu");
+  var navLogo = $("nav.menu .logo");
+  thisNav.waypoint({
+    handler: function(event, direction) {
+        thisNav.toggleClass('sticky', direction=='down');
+        navLogo.toggleClass('stuckTitle', direction=='down');
+    }
+  });
+});
+
+$(document).ready(function() {
+        var gety=function(a,y){y=0;do{y+=a.offsetTop}while(a=a.offsetParent);return y};
+        $("a[href*=#]").each(function(h){
+            if(h=this.hash)
+                this.onclick=function(){
+                $(document.body).animate({scrollTop:gety(document.getElementById(h.substring(1)))},400,function(){location.hash=h});
+                return!1;
+                };
+        });
+});
